c69641ad4c3d2e095c2408e37ec766fa5ea14645
Ruben Ravatsaas <rubrav@gmail.com>
2017-05-23 23:24:07 -0500

    Add solution and projects

---
+++ PowerManagerAPI.Tests/PowerManagerTests.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PowerManagerAPI;
+using System.ComponentModel;
+
+namespace PowerManagement.ApiWrapper.Tests
+{
+    /// <remarks>
+    /// These tests interact with the windows API, and as such they are integration tests rather than unit tests. 
+    /// Running the tests will create, change, and delete power plans, and may in some cases (errors in DeletePlan,
+    /// etc.) leave behind some artifacts. The <code>powercfg</code>command line tool tool is the simplest way to 
+    /// list and delete plans. <code>powercfg /L</code> will list all existing plans. <code>powercfg /D {GUID}</code> 
+    /// will delete it.
+    /// </remarks>
+    [TestClass]
+    public class PowerManagerTests
+    {
+
+        private Guid _balancedPlanGuid = new Guid("381b4222-f694-41f0-9685-ff5bb260df2e");
+        private Guid _nonExistentPlanGuid1 = new Guid("59e6c766-9f90-40d9-a6bf-1637db4e1943");
+        private Guid _nonExistentPlanGuid2 = new Guid("e9daf066-2cd9-4261-82b5-82d15fde51e3");
+
+        [TestMethod]
+        [ExpectedException(typeof(Win32Exception))]
+        public void DuplicatePlan_Given_NonExistentSourcePlan_Throws_Win32Exception()
+        {
+            PowerManager.DuplicatePlan(_nonExistentPlanGuid1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Win32Exception))]
+        public void DuplicatePlan_Given_PreexistingTargetPlan_ThrowsWin32Exception()
+        {
+            PowerManager.DuplicatePlan(_balancedPlanGuid, _balancedPlanGuid);
+        }
+
+        [TestMethod]
+        public void DuplicatePlan_Given_NoTargetId_Creates_NewPlan()
+        {
+            var res = PowerManager.DuplicatePlan(_balancedPlanGuid);
+            try
+            {
+                Assert.IsNotNull(res);
+                Assert.AreNotEqual(Guid.Empty, res);
+            }
+            finally
+            {
+                PowerManager.DeletePlan(res);
+            }
+        }
+
+        [TestMethod]
+        public void DuplicatePlan_Given_TargetId_Creates_NewPlan()
+        {
+            try
+            {
+                var res = PowerManager.DuplicatePlan(_balancedPlanGuid, _nonExistentPlanGuid1);
+                Assert.AreEqual(_nonExistentPlanGuid1, res);
+            }
+            finally
+            {
+                PowerManager.DeletePlan(_nonExistentPlanGuid1);
+            }
+        }
+
+        [TestMethod]
+        public void GetActivePlan_Returns_Guid()
+        {
+            var res = PowerManager.GetActivePlan();
+
+            Assert.IsNotNull(res);
+            Assert.AreNotEqual(Guid.Empty, res);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Win32Exception))]
+        public void SetActivePlan_Given_NonExitentPlan_Throws_Win32Exception()
+        {
+            PowerManager.SetActivePlan(_nonExistentPlanGuid1);
+        }
+
+        [TestMethod]
+        public void SetActivePlan_Sets_ActivePlan()
+        {
+            var sourcePlan = PowerManager.GetActivePlan();
+            Assert.IsNotNull(sourcePlan);
+            Assert.AreNotEqual(Guid.Empty, sourcePlan);
+            try
+            {
+                PowerManager.DuplicatePlan(_balancedPlanGuid, _nonExistentPlanGuid1);
+                PowerManager.SetActivePlan(_nonExistentPlanGuid1);
+
+                var active = PowerManager.GetActivePlan();
+                Assert.AreEqual(_nonExistentPlanGuid1, active);  
+            }
+            finally
+            {
+                PowerManager.SetActivePlan(sourcePlan);
+                PowerManager.DeletePlan(_nonExistentPlanGuid1);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Win32Exception))]
+        public void GetPlanName_Given_NonExistentPlanId_Throws_Win32Exception()
+        {
+            PowerManager.GetPlanName(_nonExistentPlanGuid1);
+        }
+
+        [TestMethod]
+        public void GetPlanName_Given_BalancedPlanGuid_Returns_Balanced()
+        {
+            var res = PowerManager.GetPlanName(_balancedPlanGuid);
+            Assert.AreEqual(res, "Balanced");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Win32Exception))]
+        public void SetPlanName_Given_NonExitentPlanId_Throws_Win32Exception()
+        {
+            PowerManager.SetPlanName(_nonExistentPlanGuid1, "Name");
+        }
+
+        [TestMethod]
+        public void SetPlanName_Sets_PlanName()
+        {
+            var planId = PowerManager.DuplicatePlan(_balancedPlanGuid, _nonExistentPlanGuid1);
+            try
+            {
+                var expectedName = "Name";
+
+                PowerManager.SetPlanName(planId, expectedName);
+                var actualName = PowerManager.GetPlanName(planId);
+
+                Assert.AreEqual(expectedName, actualName);
+            }
+            finally
+            {
+                PowerManager.DeletePlan(planId);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Win32Exception))]
+        public void SetPlanDescription_Given_NonExitentPlanId_Throws_Win32Exception()
+        {
+            PowerManager.SetPlanDescription(_nonExistentPlanGuid1, "Description");
+        }
+
+        [TestMethod]
+        public void SetPlanDescription_Sets_PlanDescription()
+        {
+            var planId = PowerManager.DuplicatePlan(_balancedPlanGuid, _nonExistentPlanGuid1);
+            try
+            {
+                var expectedDescription = "Description";
+
+                PowerManager.SetPlanDescription(planId, expectedDescription);
+                var actualDescription = PowerManager.GetPlanDescription(planId);
+
+                Assert.AreEqual(expectedDescription, actualDescription);
+            }
+            finally
+            {
+                PowerManager.DeletePlan(planId);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Win32Exception))]
+        public void SetPlanSetting_Given_NonExitentPlan_Throws_Win32Exception()
+        {
+            PowerManager.SetPlanSetting(_nonExistentPlanGuid1, SettingSubgroup.SYSTEM_BUTTON_SUBGROUP, Setting.LIDACTION, PowerMode.AC, 0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Win32Exception))]
+        public void SetPlanSetting_Given_ConflictingGroupAndSetting_Throws_Win32Exception()
+        {
+            var planId = PowerManager.DuplicatePlan(_balancedPlanGuid, _nonExistentPlanGuid1);
+            try
+            {
+                PowerManager.SetPlanSetting(planId, SettingSubgroup.BATTERY_SUBGROUP, Setting.LIDACTION, PowerMode.AC, 0);
+            }
+            finally
+            {
+                PowerManager.DeletePlan(planId);
+            }
+        }
+
+        [TestMethod]
+        public void SetPlanSetting_Sets_Setting()
+        {
+            var planId = PowerManager.DuplicatePlan(_balancedPlanGuid, _nonExistentPlanGuid1);
+            try
+            {
+                var subgroup = SettingSubgroup.SYSTEM_BUTTON_SUBGROUP;
+                var setting = Setting.PBUTTONACTION;
+                
+
+                uint expectedValue = 0;
+
+                PowerManager.SetPlanSetting(planId, subgroup, setting, PowerMode.AC, expectedValue);
+
+                var actualValue = PowerManager.GetPlanSetting(planId, subgroup, setting, PowerMode.AC);
+
+                Assert.AreEqual(expectedValue, actualValue);
+            }
+            finally
+            {
+                PowerManager.DeletePlan(planId);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetPlanSetting_Given_AcAndDc_Throws_ArgumentException()
+        {
+            var planId = PowerManager.DuplicatePlan(_balancedPlanGuid, _nonExistentPlanGuid1);
+            try
+            {
+                PowerManager.GetPlanSetting(planId, SettingSubgroup.SYSTEM_BUTTON_SUBGROUP, Setting.LIDACTION, PowerMode.AC | PowerMode.DC);
+            }
+            finally
+            {
+                PowerManager.DeletePlan(planId);
+            }
+        }
+    }
+}
---
+++ PowerManagerAPI/PowerManager.cs
@@ -0,0 +1,363 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Text;
+
+
+namespace PowerManagerAPI
+{
+    [Flags]
+    public enum PowerMode
+    {
+        AC = 1,
+        DC = 2,
+    }
+
+    public static class PowerManager
+    {
+        /// <summary>
+        /// Gets the currently active power plan
+        /// </summary>
+        /// <returns>Guid of the currently acvtive plan</returns>
+        public static Guid GetActivePlan()
+        {
+            IntPtr activePolicyGuidPtr = IntPtr.Zero;
+            var res = PowerGetActiveScheme(IntPtr.Zero, out activePolicyGuidPtr);
+
+            if (res != (uint)ErrorCode.SUCCESS)
+                throw new Win32Exception((int)res);
+
+            var guid = Marshal.PtrToStructure<Guid>(activePolicyGuidPtr);
+
+            LocalFree(activePolicyGuidPtr);
+
+            return guid;
+        }
+
+        /// <summary>
+        /// Sets the active power plan
+        /// </summary>
+        /// <param name="planId">The plan that should be set active.</param>
+        public static void SetActivePlan(Guid planId)
+        {
+            var res = PowerSetActiveScheme(IntPtr.Zero, ref planId);
+
+            if (res != (uint)ErrorCode.SUCCESS)
+                throw new Win32Exception((int)res);
+        }
+
+        /// <summary>
+        /// Gets the friendly name of a power plan
+        /// </summary>
+        /// <param name="planId">The Guid of the power plan</param>
+        /// <returns>Plan name</returns>
+        public static string GetPlanName(Guid planId)
+        {
+            uint bufferSize = 255;
+            IntPtr buffer =  Marshal.AllocHGlobal((int)bufferSize);
+
+            try
+            {
+                var res = PowerReadFriendlyName(IntPtr.Zero, ref planId, IntPtr.Zero, IntPtr.Zero, buffer, ref bufferSize);
+
+                if (res == (uint)ErrorCode.MORE_DATA) 
+                {
+                    // The buffer was too small. The API function has already updated the value that bufferSize points to 
+                    // to be the needed size, so all we need is to create a buffer of that size and run the API call again.
+                    Marshal.FreeHGlobal(buffer);
+                    buffer = Marshal.AllocHGlobal((int)bufferSize); 
+                    res = PowerReadFriendlyName(IntPtr.Zero, ref planId, IntPtr.Zero, IntPtr.Zero, buffer, ref bufferSize);
+                }
+
+                if (res != (uint)ErrorCode.SUCCESS)
+                    throw new Win32Exception((int)res);
+
+                return Marshal.PtrToStringUni(buffer);
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(buffer);
+            }
+        }
+
+        /// <summary>
+        /// Sets the friendly name of a power plan
+        /// </summary>
+        /// <param name="planId">The Guid of the power plan</param>
+        /// <param name="name">The new name</param>
+        public static void SetPlanName (Guid planId, string name)
+        {
+            name += char.MinValue; // Null-terminate the name string.
+            uint bufferSize = (uint)Encoding.Unicode.GetByteCount(name);
+
+            var res = PowerWriteFriendlyName(IntPtr.Zero, ref planId, IntPtr.Zero, IntPtr.Zero, name, bufferSize);
+
+            if (res != (uint)ErrorCode.SUCCESS)
+                throw new Win32Exception((int)res);
+        }
+
+        /// <summary>
+        /// Returns the description of a power plan.
+        /// </summary>
+        /// <param name="planId">Guid for the plan</param>
+        /// <returns>Description</returns>
+        public static string GetPlanDescription(Guid planId)
+        {
+            uint bufferSize = 255;
+            IntPtr buffer = Marshal.AllocHGlobal((int)bufferSize);
+
+            try
+            {
+                var res = PowerReadDescription(IntPtr.Zero, ref planId, IntPtr.Zero, IntPtr.Zero, buffer, ref bufferSize);
+
+                if (res == (uint)ErrorCode.MORE_DATA)
+                {
+                    // The buffer was too small. The API function has already updated the value that bufferSize points to 
+                    // to be the needed size, so all we need is to create a buffer of that size and run the API call again.
+                    Marshal.FreeHGlobal(buffer);
+                    buffer = Marshal.AllocHGlobal((int)bufferSize);
+                    res = PowerReadDescription(IntPtr.Zero, ref planId, IntPtr.Zero, IntPtr.Zero, buffer, ref bufferSize);
+                }
+
+                if (res != (uint)ErrorCode.SUCCESS)
+                    throw new Win32Exception((int)res);
+
+                return Marshal.PtrToStringUni(buffer);
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(buffer);
+            }
+        }
+
+        /// <summary>
+        /// Sets the description of a power plan
+        /// </summary>
+        /// <param name="planId">The Guid of the power plan</param>
+        /// <param name="description">The new description</param>
+        public static void SetPlanDescription(Guid planId, string description)
+        {
+            description += char.MinValue; // Null-terminate the description string.
+            uint bufferSize = (uint)Encoding.Unicode.GetByteCount(description);
+
+            var res = PowerWriteDescription(IntPtr.Zero, ref planId, IntPtr.Zero, IntPtr.Zero, description, bufferSize);
+
+            if (res != (uint)ErrorCode.SUCCESS)
+                throw new Win32Exception((int)res);
+        }
+
+        /// <summary>
+        /// Creates a new power plan based on the provided source plan
+        /// </summary>
+        /// <param name="sourcePlanId">The Guid for the source plan.</param>
+        /// <param name="targetPlanId">The Guid for the new plan to be created. If no guid is supplied one will be created.</param>
+        /// <returns></returns>
+        public static Guid DuplicatePlan(Guid sourcePlanId, Guid targetPlanId = new Guid())
+        {
+            if (targetPlanId == Guid.Empty)
+                targetPlanId = Guid.NewGuid();
+
+            var targetPlanPtr = Marshal.AllocHGlobal(Marshal.SizeOf(targetPlanId));
+            uint res; 
+
+            try
+            {
+                Marshal.StructureToPtr(targetPlanId, targetPlanPtr, false);
+                res = PowerDuplicateScheme(IntPtr.Zero, ref sourcePlanId, ref targetPlanPtr);
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(targetPlanPtr);
+            }
+
+            if (res != (uint)ErrorCode.SUCCESS)
+                throw new Win32Exception((int)res);
+
+            return targetPlanId;
+        }
+
+        /// <summary>
+        /// Deletes the specified power plan
+        /// </summary>
+        /// <param name="planId">Guid for the power plan to be deleted</param>
+        public static void DeletePlan(Guid planId)
+        {
+            var res = PowerDeleteScheme(IntPtr.Zero, ref planId);
+
+            if (res != (uint)ErrorCode.SUCCESS)
+                throw new Win32Exception((int)res);
+        }
+
+        /// <summary>
+        /// Gets the value for the specified power plan, power mode and setting
+        /// </summary>
+        /// <param name="plan">Guid of the power plan</param>
+        /// <param name="subgroup">The subgroup to look in</param>
+        /// <param name="setting">The settign to look up</param>
+        /// <param name="powerMode">Power mode. AC or DC, but not both.</param>
+        /// <returns></returns>
+        public static uint GetPlanSetting(Guid plan, SettingSubgroup subgroup, Setting setting, PowerMode powerMode)
+        {
+            if (powerMode == (PowerMode.AC | PowerMode.DC))
+                throw new ArgumentException("Can't get both AC and DC values at the same time, because they may be different.");
+
+            Guid subgroupId = SettingIdLookup.SettingSubgroupGuids[subgroup];
+            Guid settingId = SettingIdLookup.SettingGuids[setting];
+
+            uint value = 0;
+            uint res = 0;
+
+            if (powerMode.HasFlag(PowerMode.AC))
+            {
+                res = PowerReadACValueIndex(IntPtr.Zero, ref plan, ref subgroupId, ref settingId, out value);
+            }
+            else if (powerMode.HasFlag(PowerMode.DC))
+            {
+                res = PowerReadDCValueIndex(IntPtr.Zero, ref plan, ref subgroupId, ref settingId, out value);
+            }
+
+            if (res != (uint)ErrorCode.SUCCESS)
+                throw new Win32Exception((int)res);
+
+            return value;
+        }
+
+        /// <summary>
+        /// Alters a setting on a power plan.
+        /// </summary>
+        /// <param name="plan">The Guid for the plan you are changing</param>
+        /// <param name="subgroup">The Guid for the subgroup the setting belongs to</param>
+        /// <param name="setting">The Guid for the setting you are changing</param>
+        /// <param name="powerMode">You can chose to alter the AC value, the DC value or both using the bitwise OR operator (|) to join the flags.</param>
+        /// <param name="value">The new value for the setting. Run <code>powercfg -q</code> from the command line to list possible values</param>
+        public static void SetPlanSetting(Guid plan, SettingSubgroup subgroup, Setting setting, PowerMode powerMode, uint value)
+        {
+            Guid subgroupId = SettingIdLookup.SettingSubgroupGuids[subgroup];
+            Guid settingId = SettingIdLookup.SettingGuids[setting];
+
+            if (powerMode.HasFlag(PowerMode.AC))
+            {
+                var res = PowerWriteACValueIndex(IntPtr.Zero, ref plan, ref subgroupId, ref settingId, value);
+                if (res != (uint)ErrorCode.SUCCESS)
+                    throw new Win32Exception((int)res);
+            }
+            if (powerMode.HasFlag(PowerMode.DC))
+            {
+                var res = PowerWriteDCValueIndex(IntPtr.Zero, ref plan, ref subgroupId, ref settingId, value);
+                if (res != (uint)ErrorCode.SUCCESS)
+                    throw new Win32Exception((int)res);
+            }
+        }
+
+        #region DLL Imports
+
+        [DllImport("powrprof.dll")]
+        private static extern uint PowerGetActiveScheme(
+            [In, Optional] IntPtr UserPowerKey,
+            [Out] out IntPtr ActivePolicyGuid
+        );
+
+        [DllImport("powrprof.dll")]
+        private static extern uint PowerSetActiveScheme(
+            [In, Optional] IntPtr UserPowerKey,
+            [In] ref Guid ActivePolicyGuid
+        );
+
+        [DllImport("powrprof.dll")]
+        private static extern uint PowerDuplicateScheme(
+            [In, Optional] IntPtr RootPowerKey,
+            [In] ref Guid SourceSchemeGuid,
+            [In] ref IntPtr DestinationSchemeGuid
+        );
+
+        [DllImport("powrprof.dll")]
+        private static extern uint PowerDeleteScheme(
+            [In, Optional]  IntPtr      RootPowerKey,
+            [In]            ref Guid    SchemeGuid
+        );
+
+        [DllImport("powrprof.dll")]
+        private static extern uint PowerReadFriendlyName(
+            [In, Optional]  IntPtr      RootPowerKey,
+            [In, Optional]  ref Guid    SchemeGuid,
+            [In, Optional]  IntPtr      SubGroupOfPowerSettingsGuid,
+            [In, Optional]  IntPtr      PowerSettingGuid,
+            [Out, Optional] IntPtr      Buffer,
+            [In, Out]       ref uint    BufferSize
+        );
+
+        [DllImport("powrprof.dll", CharSet = CharSet.Unicode)]
+        private static extern uint PowerWriteFriendlyName(
+            [In, Optional]  IntPtr      RootPowerKey,
+            [In]            ref Guid    SchemeGuid,
+            [In, Optional]  IntPtr      SubGroupOfPowerSettingsGuid,
+            [In, Optional]  IntPtr      PowerSettingGuid,
+            [In]            string      Buffer,
+            [In]            UInt32      BufferSize
+        );
+
+        [DllImport("powrprof.dll")]
+        private static extern uint PowerReadDescription(
+            [In, Optional]  IntPtr      RootPowerKey,
+            [In, Optional]  ref Guid    SchemeGuid,
+            [In, Optional]  IntPtr      SubGroupOfPowerSettingsGuid,
+            [In, Optional]  IntPtr      PowerSettingGuid,
+            [Out, Optional] IntPtr      Buffer,
+            [In, Out]       ref uint    BufferSize
+        );
+
+        [DllImport("powrprof.dll", CharSet = CharSet.Unicode)]
+        private static extern uint PowerWriteDescription(
+            [In, Optional]  IntPtr      RootPowerKey,
+            [In]            ref Guid    SchemeGuid,
+            [In, Optional]  IntPtr      SubGroupOfPowerSettingsGuid,
+            [In, Optional]  IntPtr      PowerSettingGuid,
+            [In]            string      Buffer,
+            [In]            UInt32      BufferSize
+        );
+
+        [DllImport("powrprof.dll")]
+        private static extern uint PowerReadACValueIndex(
+            [In, Optional]  IntPtr      RootPowerKey,
+            [In, Optional]  ref Guid    SchemeGuid,
+            [In, Optional]  ref Guid    SubGroupOfPowerSettingsGuid,
+            [In, Optional]  ref Guid    PowerSettingGuid,
+            [Out]           out uint    AcValueIndex
+        );
+
+        [DllImport("powrprof.dll")]
+        private static extern uint PowerWriteACValueIndex(
+            [In, Optional]  IntPtr      RootPowerKey,
+            [In]            ref Guid    SchemeGuid,
+            [In, Optional]  ref Guid    SubGroupOfPowerSettingsGuid,
+            [In, Optional]  ref Guid    PowerSettingGuid,
+            [In]            uint        AcValueIndex
+        );
+
+        [DllImport("powrprof.dll")]
+        private static extern uint PowerReadDCValueIndex(
+            [In, Optional]  IntPtr      RootPowerKey,
+            [In, Optional]  ref Guid    SchemeGuid,
+            [In, Optional]  ref Guid    SubGroupOfPowerSettingsGuid,
+            [In, Optional]  ref Guid    PowerSettingGuid,
+            [Out]           out uint    DcValueIndex
+        );
+
+        [DllImport("powrprof.dll")]
+        private static extern uint PowerWriteDCValueIndex(
+            [In, Optional]  IntPtr      RootPowerKey,
+            [In]            ref Guid    SchemeGuid,
+            [In, Optional]  ref Guid    SubGroupOfPowerSettingsGuid,
+            [In, Optional]  ref Guid    PowerSettingGuid,
+            [In]            uint        DcValueIndex
+        );
+
+
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr LocalFree(
+            [In]            IntPtr      hMem
+        );
+        #endregion
+    }
+}
